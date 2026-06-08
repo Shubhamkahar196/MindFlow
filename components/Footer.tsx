bde9d0192ff8998494d2990316809dda5ea2260f
Shubham kahar <Shubhamkahar196@gmail.com>
2026-06-08 21:59:51 +0530

    added footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,27 +1,44 @@
-import React from 'react'
-import { House } from 'lucide-react';
-import { Hourglass } from 'lucide-react';
-import Image from 'next/image';
+import React from 'react';
+import { House, Hourglass, CheckSquare, Timer } from 'lucide-react';
+
 const Footer = () => {
   return (
-    <div>
-        
-        <ul>
-            <ol>
-                <li>Todo</li>
-                <li>
-                    <House/>
-                </li>
-                <li>
-                    <Hourglass/>
-                </li>
-                <li>Timer</li>
+    <footer className="fixed bottom-4 left-1/2 z-50 w-[90%] max-w-md -translate-x-1/2 rounded-2xl border border-white/10 bg-black/60 p-3 shadow-xl backdrop-blur-md">
+      <ul className="flex items-center justify-around gap-2">
+        {/* Todo Item */}
+        <li>
+          <a href="#todo" className="flex flex-col items-center gap-1 text-xs font-medium text-zinc-400 transition-colors duration-200 hover:text-white">
+            <CheckSquare className="h-5 w-5" />
+            <span>Todo</span>
+          </a>
+        </li>
 
-            </ol>
-        </ul>
-        
-        </div>
-  )
-}
+        {/* House Item */}
+        <li>
+          <a href="#home" className="flex flex-col items-center gap-1 text-xs font-medium text-zinc-400 transition-colors duration-200 hover:text-white">
+            <House className="h-5 w-5" />
+            <span>Home</span>
+          </a>
+        </li>
 
-export default Footer
+        {/* Hourglass Item */}
+        <li>
+          <a href="#history" className="flex flex-col items-center gap-1 text-xs font-medium text-zinc-400 transition-colors duration-200 hover:text-white">
+            <Hourglass className="h-5 w-5" />
+            <span>History</span>
+          </a>
+        </li>
+
+        {/* Timer Item */}
+        <li>
+          <a href="#timer" className="flex flex-col items-center gap-1 text-xs font-medium text-zinc-400 transition-colors duration-200 hover:text-white">
+            <Timer className="h-5 w-5" />
+            <span>Timer</span>
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
