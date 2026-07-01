4c035f659bb5cf3c837854eaa9ca472913fa9fc7
Shubham kahar <Shubhamkahar196@gmail.com>
2026-07-01 23:46:30 +0530

    updated the usermodel

--- models/user.ts
+++ models/user.ts
@@ -3,22 +3,19 @@ import mongoose,  {  Schema } from "mongoose";
 
 const userSchema = new Schema({
      
-    fullName :{
-        type:String,
-        required: true,
-        trim: true,
-
-    },
-    email:{
-        type:String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type:String,
-        required: true,
-    }
-})
+   email: {
+    type:String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+   },
+   provide: {
+    type: String,
+    required: true,
+    default: "google"
+   }
+},{timestamps: true})
 
 const User = mongoose.models.User ||  mongoose.model("User",userSchema);
 
